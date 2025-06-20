be96f8765398596b16a6d5ee18114431185f11c3
糖饼 <sugarpie.tang@gmail.com>
2025-06-20 23:26:04 +0800

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,15 @@ export default {
   roots: ['<rootDir>/src'],
   testMatch: ['**/(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   transformIgnorePatterns: ['/node_modules/'],
   testEnvironment: 'miniflare',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
+  globals: {},
 };
