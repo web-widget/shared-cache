231720dec5b8b2e4a4c3938456b1b3bdec0e1936
糖饼 <sugarpie.tang@gmail.com>
2025-06-20 12:38:01 +0800

    style: test

--- src/utils/vary.test.ts
+++ src/utils/vary.test.ts
@@ -3,14 +3,14 @@ import { vary } from './vary';
 describe('vary(res, field)', function () {
   describe('arguments', function () {
     describe('res', function () {
-      test('should be required', function () {
+      it('should be required', function () {
         expect(() => {
           // @ts-expect-error - headers argument is required
           vary();
         }).toThrow('headers argument is required');
       });
 
-      test('should not allow non-res-like objects', function () {
+      it('should not allow non-res-like objects', function () {
         expect(() => {
           // @ts-expect-error - headers argument is required
           vary(null, {});
@@ -20,25 +20,25 @@ describe('vary(res, field)', function () {
   });
 
   describe('when no Vary', function () {
-    test('should set value', function () {
+    it('should set value', function () {
       const headers = new Headers();
       vary(headers, 'Origin');
       expect(headers.get('Vary')).toBe('Origin');
     });
 
-    test('should set value with multiple calls', function () {
+    it('should set value with multiple calls', function () {
       const headers = new Headers();
       vary(headers, ['Origin', 'User-Agent']);
       expect(headers.get('Vary')).toBe('Origin, User-Agent');
     });
 
-    test('should preserve case', function () {
+    it('should preserve case', function () {
       const headers = new Headers();
       vary(headers, ['ORIGIN', 'user-agent', 'AccepT']);
       expect(headers.get('Vary')).toBe('ORIGIN, user-agent, AccepT');
     });
 
-    test('should not set Vary on empty array', function () {
+    it('should not set Vary on empty array', function () {
       const headers = new Headers();
       vary(headers, []);
       expect(headers.get('Vary')).toBe(null);
@@ -46,14 +46,14 @@ describe('vary(res, field)', function () {
   });
 
   describe('when existing Vary', function () {
-    test('should set value', function () {
+    it('should set value', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept');
       vary(headers, 'Origin');
       expect(headers.get('Vary')).toBe('Accept, Origin');
     });
 
-    test('should set value with multiple calls', function () {
+    it('should set value with multiple calls', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept');
       vary(headers, 'Origin');
@@ -61,21 +61,21 @@ describe('vary(res, field)', function () {
       expect(headers.get('Vary')).toBe('Accept, Origin, User-Agent');
     });
 
-    test('should not duplicate existing value', function () {
+    it('should not duplicate existing value', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept');
       vary(headers, 'Accept');
       expect(headers.get('Vary')).toBe('Accept');
     });
 
-    test('should compare case-insensitive', function () {
+    it('should compare case-insensitive', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept');
       vary(headers, 'accEPT');
       expect(headers.get('Vary')).toBe('Accept');
     });
 
-    test('should preserve case', function () {
+    it('should preserve case', function () {
       const headers = new Headers();
       headers.set('Vary', 'AccepT');
       vary(headers, ['accEPT', 'ORIGIN']);
@@ -84,14 +84,14 @@ describe('vary(res, field)', function () {
   });
 
   describe('when existing Vary as array', function () {
-    test('should set value', function () {
+    it('should set value', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept, Accept-Encoding');
       vary(headers, 'Origin');
       expect(headers.get('Vary')).toBe('Accept, Accept-Encoding, Origin');
     });
 
-    test('should not duplicate existing value', function () {
+    it('should not duplicate existing value', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept, Accept-Encoding');
       vary(headers, 'Accept');
@@ -100,14 +100,14 @@ describe('vary(res, field)', function () {
   });
 
   describe('when Vary: *', function () {
-    test('should set value', function () {
+    it('should set value', function () {
       const headers = new Headers();
       headers.set('Vary', '*');
       vary(headers, 'Origin');
       expect(headers.get('Vary')).toBe('*');
     });
 
-    test('should act as if all values alread set', function () {
+    it('should act as if all values already set', function () {
       const headers = new Headers();
       headers.set('Vary', '*');
       vary(headers, 'Origin');
@@ -115,14 +115,14 @@ describe('vary(res, field)', function () {
       expect(headers.get('Vary')).toBe('*');
     });
 
-    test('should erradicate existing values', function () {
+    it('should eradicate existing values', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept, Accept-Encoding');
       vary(headers, '*');
       expect(headers.get('Vary')).toBe('*');
     });
 
-    test('should update bad existing header', function () {
+    it('should update bad existing header', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept, Accept-Encoding, *');
       vary(headers, 'Origin');
@@ -131,26 +131,26 @@ describe('vary(res, field)', function () {
   });
 
   describe('when field is string', function () {
-    test('should set value', function () {
+    it('should set value', function () {
       const headers = new Headers();
       vary(headers, 'Accept');
       expect(headers.get('Vary')).toBe('Accept');
     });
 
-    test('should set value when vary header', function () {
+    it('should set value when vary header', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept, Accept-Encoding');
       vary(headers, 'Origin');
       expect(headers.get('Vary')).toBe('Accept, Accept-Encoding, Origin');
     });
 
-    test('should acept LWS', function () {
+    it('should accept LWS', function () {
       const headers = new Headers();
       vary(headers, '  Accept     ,     Origin    ');
       expect(headers.get('Vary')).toBe('Accept, Origin');
     });
 
-    test('should handle contained *', function () {
+    it('should handle contained *', function () {
       const headers = new Headers();
       vary(headers, 'Accept,*');
       expect(headers.get('Vary')).toBe('*');
@@ -158,31 +158,31 @@ describe('vary(res, field)', function () {
   });
 
   describe('when field is array', function () {
-    test('should set value', function () {
+    it('should set value', function () {
       const headers = new Headers();
       vary(headers, ['Accept', 'Accept-Language']);
       expect(headers.get('Vary')).toBe('Accept, Accept-Language');
     });
 
-    test('should ignore double-entries', function () {
+    it('should ignore double-entries', function () {
       const headers = new Headers();
       vary(headers, ['Accept', 'Accept']);
       expect(headers.get('Vary')).toBe('Accept');
     });
 
-    test('should be case-insensitive', function () {
+    it('should be case-insensitive', function () {
       const headers = new Headers();
       vary(headers, ['Accept', 'ACCEPT']);
       expect(headers.get('Vary')).toBe('Accept');
     });
 
-    test('should handle contained *', function () {
+    it('should handle contained *', function () {
       const headers = new Headers();
       vary(headers, ['Origin', 'User-Agent', '*', 'Accept']);
       expect(headers.get('Vary')).toBe('*');
     });
 
-    test('should handle existing values', function () {
+    it('should handle existing values', function () {
       const headers = new Headers();
       headers.set('Vary', 'Accept, Accept-Encoding');
       vary(headers, ['origin', 'accept', 'accept-charset']);
